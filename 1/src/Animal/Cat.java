24d625b49c868ec86ce5778048141a50fe50bd84
Maaasta <slxooxls@gmail.com>
2019-06-21 02:04:57 +0200

    Initial Commit

---
+++ 1/src/Animal/Cat.java
@@ -0,0 +1,10 @@
+package Animal;
+
+public class Cat
+{
+public static void main(String args[])
+{
+    System.out.println("To jest Kot");
+}
+
+}
---
+++ 1/src/Main.java
@@ -0,0 +1,11 @@
+import Animal.Cat;
+import Animal.Dog;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+    }
+}
